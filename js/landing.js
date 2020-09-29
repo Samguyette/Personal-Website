c3c8b32177bce4440a2da26af4b8e76485bbb1a5
Sam Guyette <samguyette@gmail.com>
2020-09-28 17:47:20 -0700

    movement change

---
+++ js/landing.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+	$(window).scroll(function(){
+		var scrollPercent = 100 / $(this).scrollTop()
+		if(scrollPercent > 1 || scrollPercent < 0){
+			scrollPercent = 1;
+		}
+		$(".hero").css({"opacity" : scrollPercent})
+	})
+});
+$(document).ready(function (){
+	$('.test, .nav-link, .navbar-brand, .new-button').click(function() {
+	    var sectionTo = $(this).attr('href');
+	    $('html, body').animate({
+	      scrollTop: $(sectionTo).offset().top+-50
+	    }, 1200);
+	});
+});
